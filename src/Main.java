f8c0759d4f7adf70c8893d27219fe12ce9a3ea15
Alex <Alex@192.168.0.100>
2016-11-06 10:29:38 +0200

    Сделал Ctrl+Shift+F  в домашеннме задании №11.145 в) Условие: дан массив из n%2=0 эл-ов. Поменять местами его половины след. об.: 1й эл. с последним, 2й с предпоследним...

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
 import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int [] mas = new int [10];
-		
-	    for (int i=0; i < mas.length; i++ ){
-	    	mas [i] = (int) (Math.random()*11); 
-	    	System.out.print(" | "+mas[i] +" |");
-	    } for(int j=0; j<mas.length; j=j+2 ){ 
-	    	int d = mas.length-1;
-	    	int temp = mas[j];
-	        mas[j] = mas[d-j];
-	        mas [d-j] = temp;
-	    }	    	
-	    System.out.print("\nCopy   = ");
+
+		int[] mas = new int[10];
+
+		for (int i = 0; i < mas.length; i++) {
+			mas[i] = (int) (Math.random() * 11);
+			System.out.print(" | " + mas[i] + " |");
+		}
+		for (int j = 0; j < mas.length; j = j + 2) {
+			int d = mas.length - 1;
+			int temp = mas[j];
+			mas[j] = mas[d - j];
+			mas[d - j] = temp;
+		}
+		System.out.print("\nCopy   = ");
 		System.out.println(Arrays.toString(mas));
 
-	   	}
 	}
-
-	
+}
